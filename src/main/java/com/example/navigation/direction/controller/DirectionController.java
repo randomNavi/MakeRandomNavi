75fbcf8f84a0e1ed492fef2ce1ab4b7a7f7f09b5
강현욱 <bug6868@naver.com>
2023-10-12 20:58:09 +0900

    혼자쓰려고 만들어봄

--- src/main/java/com/example/navigation/direction/controller/DirectionController.java
+++ src/main/java/com/example/navigation/direction/controller/DirectionController.java
@@ -8,16 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 // 네비 컨트롤러
-public class DIrectionController {
+public class DirectionController {
 
     private final DirectionService mainService;
 
     @GetMapping("/")
-    public String main(){
+    public String home(){
         return "index";
     }
-
-
-
 
 }
