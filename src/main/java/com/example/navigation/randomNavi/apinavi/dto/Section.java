d6fd4432315a40600e0717607c2ca626cab0ca61
jay <daonj0715@gmail.com>
2023-10-13 15:39:24 +0900

    refush because of merging fail

--- src/main/java/com/example/navigation/randomNavi/apinavi/dto/Section.java
+++ src/main/java/com/example/navigation/randomNavi/apinavi/dto/Section.java
@@ -1,21 +1,21 @@
-package com.example.navigation.randomNavi.apinavi.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Section {
-
-    @JsonProperty("distance")
-    private int distance;
-
-    @JsonProperty("duration")
-    private int duration;
-
-    @JsonProperty("bound")
-    private Bound bound;
-}
+//package com.example.navigation.randomNavi.apinavi.dto;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Section {
+//
+//    @JsonProperty("distance")
+//    private int distance;
+//
+//    @JsonProperty("duration")
+//    private int duration;
+//
+//    @JsonProperty("bound")
+//    private Bound bound;
+//}
