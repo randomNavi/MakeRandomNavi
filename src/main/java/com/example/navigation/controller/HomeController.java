e1031f5cdef5eb283d94d095c79fb1d8c256707a
강현욱 <bug6868@naver.com>
2023-10-16 10:09:36 +0900

    dev-api에서 복붙해옴, 알고리즘 실험용

---
+++ src/main/java/com/example/navigation/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.navigation.controller;
+
+import com.example.navigation.user.service.RandomUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Slf4j(topic = "Home Controller")
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    private final RandomUserService userService;
+
+//    @GetMapping("/")
+//    public String hello(@CookieValue(required = false, value = "Authorization_Access") String access,
+//                        Model model,
+//                        HttpServletResponse response) throws Exception {
+//        var user = userService.getUserInfoWithToken(access, response);
+//
+//        model.addAttribute("nickname", user.getKakao_account().getProfile().getNickname());
+//        return "index";
+//
+//    }
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("message", "안녕하세요!");
+        return "index"; // 뷰 이름은 templates 폴더에 있는 템플릿 파일명과 일치해야 합니다.
+    }
+
+    @GetMapping("/navigation")
+    public String showNavigation() {
+        return "navigation";
+    }
+}
